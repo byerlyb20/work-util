8cf54cf646d0998e998ae88d7bb1c0e5256893a6
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2021-05-28 17:06:42 -0400

    Webb spider: parse according to bill pay screen, not invoice screen

--- spiders/webb_spider.py
+++ spiders/webb_spider.py
@@ -1,27 +1,26 @@
 import scrapy
 
+
 class WebbSpider(scrapy.Spider):
     name = 'webb_invoices'
     start_urls = [
-        'file:///path/to/file.html',
+        'file:///Users/brighambyerly/Downloads/statement.html',
     ]
 
-    def parse(self, response):
-        for invoice in response.xpath("//form[@id='invoiceExportCSV']/div[@class='searchDataList']"):
-            invoice_id = invoice.xpath("./div[1]/b/a/text()").get()
-            invoice_date = invoice.xpath("./div[4]/p[1]/text()").get().split(':')[1].strip()
-
-            order_date = invoice.xpath("./div[4]/p[2]/text()").get().split(':')[1].strip()
-            order_num = invoice.xpath("./div[4]/p[3]/text()").get().split(':')[1].strip()
-            po_num = invoice.xpath("./div[6]/p[1]/text()").get().split(':')[1].strip()
-
-            amount = invoice.xpath("./div[7]/p[1]/text()")[3].get().strip()
-
-            yield {
-                'invoice_id': invoice_id, 'invoice_date': invoice_date, 'order_date': order_date,
-                'order_num': order_num, 'po_num': po_num, 'amount': amount
-            }
-
-        #next_page = response.css('li.next a::attr("href")').get()
-        #if next_page is not None:
-        #    yield response.follow(next_page, self.parse)
+    def parse(self, response, **kwargs):
+        dropdown_ids = ["Current", "30Days", "60Days", "90Days", "120Days"]
+        dropdowns = map(lambda d: response.xpath(f"//*[@id='{d}']"), dropdown_ids)
+        for dropdown in dropdowns:
+            # Two rows correspond to one invoice
+            invoice = {}
+            rows = dropdown.xpath(".//tr")
+            for i, row in enumerate(rows):
+                if i % 2 == 0:
+                    invoice['invoice_id'] = row.xpath("./th/div[1]/a/text()").get()
+                    invoice['balance'] = row.xpath("./th/div[2]/text()").get().split(':')[1].strip()
+                else:
+                    invoice['invoice_date'] = row.xpath("./td/div[1]/p/text()").get().split(':')[1].strip()
+                    invoice['order_num'] = row.xpath("./td/div[2]/p/text()").get().split(':')[1].strip()
+                    invoice['job_name'] = row.xpath("./td/div[3]/p/text()").get().split(':')[1].strip()
+                    invoice['po_num'] = row.xpath("./td/div[4]/p/text()").get().split(':')[1].strip()
+                    yield invoice
